b977ac9e93ce8723a7ea5c864afe139e1f7707c3
takahiro_aoki <takahiro.aoki.0902@gmail.com>
2024-06-17 08:14:54 +0000

    ref function

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,26 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const price = ref(0)
+const incrementPrice = (): void => {
+  price.value += 1
+}
+
+const info = ref({
+  'count': 0
+})
+const incrementCount = (): void => {
+  info.value.count += 1
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p>Price: {{ price }}</p>
+  <button @click="incrementPrice">increment price</button>
+
+  <p>Count: {{ info.count }}</p>
+  <button @click="incrementCount">increment count</button>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
